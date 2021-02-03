004d9d2cf46183d7a2364823237263b6d45a0f8a
url54 <70337292+url54@users.noreply.github.com>
2021-02-03 18:33:49 -0500

    Update json_retrieving.py

--- json_retrieving.py
+++ json_retrieving.py
@@ -1,3 +1,10 @@
+# This program was created for an assignment.  The assignment requires us to visit and pull JSON data from a website.  Then to sum up all the integar values in that JSON
+# file together.  The original instructions are included below this text.
+
+# The program will prompt for a URL, http://py4e-data.dr-chuck.net/comments_1139135.json, read the JSON data from that URL using requests and then parse and extract the
+# comment counts from the JSON data, compute the sum of the numbers in the file and enter the sum below:
+
+
 import requests
 import json
 import re
